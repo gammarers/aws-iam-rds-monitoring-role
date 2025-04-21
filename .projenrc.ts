9700196d9833a8b9430429794441c4458140b4e2
yicr <yicr@users.noreply.github.com>
2025-04-21 19:36:46 +0900

    chore: deps upgrade workflow schedule to monthly

--- .projenrc.ts
+++ .projenrc.ts
@@ -17,7 +17,7 @@ const project = new awscdk.AwsCdkConstructLibrary({
   depsUpgradeOptions: {
     workflowOptions: {
       labels: ['auto-approve', 'auto-merge'],
-      schedule: javascript.UpgradeDependenciesSchedule.expressions(['9 16 * * 3']), // everyday
+      schedule: javascript.UpgradeDependenciesSchedule.expressions(['9 16 8 * +']), // everyday
     },
   },
   minNodeVersion: '18.0.0',
